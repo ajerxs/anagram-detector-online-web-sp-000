d8c1af6c3cf0580eb056f2f77e068611554e2f08
Andrew Eames <ajerxs@gmail.com>
2020-11-28 23:33:14 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :anagrams
+
+  def initialize
+    @anagrams = anagrams
+  end
+
+  def match
+    nuarray = []
+    anagrams.each do |words|
+      words.split(" ").sort
+      nuarray << words
+    end
+    if nuarray = anagrams
+      return anagrams
+    end
+  end
+end
